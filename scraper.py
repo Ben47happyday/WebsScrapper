020e72503d12aeef6a9569d6cf7dec6d7b5eb462
ben.zhou <ben.zhou@teeg.com>
2020-02-20 17:54:10 +1100

    feat: capture sell price and save list into json

--- scraper.py
+++ scraper.py
@@ -6,29 +6,73 @@ import json
 import re 
 
 
-num = 1
+_min = 1000000
+_max = 1200000
+_inc = 100000
 
-url = 'https://www.domain.com.au/sale/wahroonga-nsw-2076/?excludeunderoffer=1'
-response = requests.get(url)
-soup = BeautifulSoup(response.text,"html.parser")
+_page = 1
+_suburb = 'wahroonga'
 
-tags = soup.findAll("link", {"itemprop":"url"})
+_resultContent = []
+
+for i in range (_min,_max,_inc):
+
+    _start_price = i - _inc
+    _end_price = i 
+    _page_end = False 
+    _page = 1
+
+    while _page_end == False:
+
+        url = f"https://www.domain.com.au/sale/{_suburb}-nsw-2076/?price={_start_price}-{_end_price}&enablemobilemap=1&page={_page}"
+
+     
+        
+        response = requests.get(url)
+
+        if response.status_code == 400:
+            _page_end = True
+            print("response status {}".format(response.status_code)) 
+            break
 
 
-for eachLink in tags: 
-    propertyurl = eachLink['href']
-    print ("Property URL: "+ propertyurl)
-    propertyreponse = requests.get(propertyurl)
-    propertysoup = BeautifulSoup(propertyreponse.text,'html.parser')
-    propertytags = propertysoup.findAll('script', text = re.compile('window\[\'__domain_group/APP_PROPS\'\]') )
-    tag_string = str(propertytags[0].contents[0]) 
 
-    json_string =tag_string.replace("window[\'__domain_group/APP_PROPS\'] =","").replace("; window['__domain_group/APP_PAGE'] = 'listing-details'",'')
+        soup = BeautifulSoup(response.text,"html.parser")
 
-    propertyjson = json.loads(json_string)
-    name = propertyjson["address"].replace ('/','_').replace('-','_')
+        tags = soup.findAll("link", {"itemprop":"url"})
+        if tags == []:
+            break
+        
+        print ("\nURL: {}\npage number: {}\nprice range {} - {}".format(url,_page,_start_price,_end_price) )
 
-    print("Property address: {} guid price: {} bedroom: {} , )
+        for eachLink in tags: 
+            propertyurl = eachLink['href']
 
-    # with open(f"properties\{name}.json","w") as j:
-    #     json.dump(propertyjson,j)
+            print ("   Property URL: "+ propertyurl)
+
+            propertyreponse = requests.get(propertyurl)
+            propertysoup = BeautifulSoup(propertyreponse.text,'html.parser')
+            propertytags = propertysoup.findAll('script', text = re.compile('window\[\'__domain_group/APP_PROPS\'\]') )
+            tag_string = str(propertytags[0].contents[0]) 
+
+            json_string =tag_string.replace("window[\'__domain_group/APP_PROPS\'] =","").replace("; window['__domain_group/APP_PAGE'] = 'listing-details'",'')
+
+            propertyjson = json.loads(json_string)
+            add = propertyjson["address"].replace ('/','_').replace('-','_')
+            p_id = propertyjson["id"]
+            beds = propertyjson["beds"]
+            propertyType = propertyjson["propertyType"]
+            #price = propertyjson["listingsMap"][f"{p_id}"]["listingModel"]["price"]
+            price = (_end_price + _start_price) / 2 
+
+            
+            _resultContent.append({"URL":url,"address":add, "price":price, "beds":beds, "type":propertyType})  
+
+            # result = "\n URL: {} \n Property address: {} \n guid price: {} \n bedroom: {} \n type: {} ".format(propertyurl,add,price,beds,propertyType) 
+
+            # _resultContent += result
+            
+            _page += 1 
+
+with open(f"result.json","w") as j:
+        json.dump(_resultContent,j)
